08c6297130b81dbe83ede8860b83aae3e474e3e7
Abbas.khalili <khalilikhoo2@gmail.com>
2018-11-27 11:53:41 +0330

    Update build.sh

--- build.sh
+++ build.sh
@@ -115,4 +115,4 @@ if [ ! -f "$CAKE_EXE" ]; then
 fi
 
 # Start Cake
-exec mono "$CAKE_EXE" $BUILD_DIR/$SCRIPT "${CAKE_ARGUMENTS[@]}"
+exec mono "$CAKE_EXE" "$BUILD_DIR/$SCRIPT" "${CAKE_ARGUMENTS[@]}"
