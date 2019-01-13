a4cf3b9ad40df0cdd40c6e50f662c49028ee3f80
Abbas <khalilikhoo2@gmail.com>
2019-01-13 22:59:19 +0330

    Added Addtive Facade Service unit test

---
+++ Gravity/Geotechnic.Facade.Services.Tests.Unit/AdditiveFacadeServiceTests.cs
@@ -0,0 +1,78 @@
+using FluentAssertions;
+using Geotechnic.Domain.Additives;
+using Geotechnic.Facade.Contracts.Additives.Commands;
+using Gravity.Application;
+using Xunit;
+
+namespace Geotechnic.Facade.Services.Tests.Unit
+{
+    public class AdditiveFacadeServiceTests : InMemoryAdditiveFacadeService
+    {
+        private readonly EntityIdBuilder<AdditiveId> _idBuilder;
+
+        public AdditiveFacadeServiceTests()
+        {
+            _idBuilder = new EntityIdBuilder<AdditiveId>();
+        }
+
+        private const long BranchId = 100;
+        private const long Id = 1;
+        private const string Title = "Wdss 5002";
+
+        [Fact]
+        public void Create_should_add_Additive_to_repository_by_commandBus()
+        {
+            DispatchAnAdditiveCreate();
+
+            var id = _idBuilder.WithId(Id).Build();
+            var expectedResult = Repository.Get(id);
+
+            expectedResult.Id.Should().BeEquivalentTo(id);
+            expectedResult.BranchId.Should().Be(BranchId);
+            expectedResult.Title.Should().Be(Title);
+        }
+
+        [Fact]
+        public void Update_should_modify_Additive_in_repository_by_commandBus()
+        {
+            DispatchAnAdditiveCreate();
+
+            var title = "Az90";
+            var updateCommand = new AdditiveUpdate()
+                { BranchId = BranchId, Id = Id, Title = title };
+            Bus.Dispatch(updateCommand);
+
+            var id = _idBuilder.WithId(Id).Build();
+            var expectedResult = Repository.Get(id);
+
+            expectedResult.Id.Should().BeEquivalentTo(id);
+            expectedResult.BranchId.Should().Be(BranchId);
+            expectedResult.Title.Should().Be(title);
+        }
+
+        [Fact]
+        public void Delete_should_remove_Additive_from_repository_by_commandBus()
+        {
+            DispatchAnAdditiveCreate();
+
+            var deleteCommand = new AdditiveDelete()
+                { BranchId = BranchId, Id = Id };
+            Bus.Dispatch(deleteCommand);
+
+            var id = _idBuilder.WithId(Id).Build();
+            var expectedResult = Repository.Get(id);
+
+            expectedResult.Should().BeNull();
+        }
+
+        private void DispatchAnAdditiveCreate()
+        {
+            var command = new AdditiveCreate()
+            {
+                BranchId = BranchId,
+                Title = Title
+            };
+            Bus.Dispatch(command);
+        }
+    }
+}
---
+++ Gravity/Geotechnic.Facade.Services.Tests.Unit/InMemoryAdditiveFacadeService.cs
@@ -0,0 +1,27 @@
+using Geotechnic.Application.CommandHandlers;
+using Geotechnic.Domain.Additives;
+using Geotechnic.Domain.OrderConcrete;
+using Geotechnic.Facade.Contracts.Additives.Commands;
+using Geotechnic.Persistence.Mappings;
+using Geotechnic.Persistence.Repositories;
+using Gravitest.NHibernate;
+using Gravity.Application;
+using Gravity.NHibernate;
+
+namespace Geotechnic.Facade.Services.Tests.Unit
+{
+    public class InMemoryAdditiveFacadeService : InMemoryDatabase
+    {
+        public IAdditiveRepository Repository;
+        public ICommandBus Bus;
+
+        public InMemoryAdditiveFacadeService() : base(typeof(AdditiveMapping).Assembly)
+        {
+            ISequenceHelper sequenceHelper = new FakeSequenceHelper();
+            Repository = new AdditiveRepository(Session, sequenceHelper);
+            IOrderRepository orderRepository = new OrderRepository(Session, sequenceHelper);
+            ICommandHandler<AdditiveCreate> commandHandler = new AdditiveCommandHandler(Repository, orderRepository);
+            Bus = new CommandBusFake<AdditiveCreate>(commandHandler, Session);
+        }
+    }
+}
